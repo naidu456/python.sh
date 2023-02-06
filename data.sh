09b6fc68915881ec1d371cdb6b95f803cabf702c
naidu456 <118648202+naidu456@users.noreply.github.com>
2023-02-06 14:50:58 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get full upgrade -y
-sudo apt-get install python3-pip -y
+sudo yum update
+sudo yum install python3-pip -y
 git clone https://github.com/shalu212/Agri.git
 cd Agri
 pip3 install -r requirements.txt
